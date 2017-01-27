e0c504793b1fdb7084ef483eb1030d81f6213ab1
Jesse Li <jessejesse123@gmail.com>
2017-01-26 20:35:42 -0500

    remove inputs, parse file on click

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-function readFile(e) {
-  var file = e.target.files[0];
-  if (file) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var contents = e.target.result;
-      display(contents);
-    };
-    reader.readAsText(file);
-  }
-}
-
-function display(contents) {
-  colors = getColors("termite", contents);
+function display(format, contents) {
+  colors = getColors(format, contents);
   if (colors) {
-    $("#file-input").style.display = "none";
+    colors.forEach(function(color) {
+      console.log(color);
+    })
   } else {
     console.log("couldn't read");
   }
 }
 
-$(document).ready(function() {
-  $("#file-input").on("change", function(e) {
-    readFile(e);
-  })
+$("#submit").click(function(event) {
+  format = $("#format").val();
+  contents = $("#contents").val();
+  $("#file-input").remove();
+  display(format, contents);
 });
